5785d9359ed6beeda5e2f1db237fa3c306e2c452
stohirov <suxrobtoxirov001@gmail.com>
2025-01-05 01:08:40 +0500

    Coding Bat continue of warmup

--- src/solutions/codingbat/WarmUp1.java
+++ src/solutions/codingbat/WarmUp1.java
@@ -47,4 +47,77 @@ public class WarmUp1 {
         return str.substring(0, n) + str.substring(n + 1);
     }
 
+    public String frontBack(String str) {
+        int length = str.length();
+        if (length <= 1) return str;
+        String mid = str.substring(1, length - 1);
+        return str.charAt(length - 1) + mid + str.charAt(0);
+    }
+
+    public String front3(String str) {
+        int i = 0;
+        StringBuilder res = new StringBuilder();
+        while (i < 3) {
+            if (str.length() >= 3) {
+                res.append(str, 0, 3);
+            } else {
+                res.append(str);
+            }
+            i++;
+        }
+        return res.toString();
+    }
+
+    public String backAround(String str) {
+        return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
+    }
+
+    public boolean or35(int n) {
+        return n % 3 == 0 || n % 5 == 0;
+    }
+
+    public String front22(String str) {
+        if (str.length() < 2) return str + str + str;
+        else return str.substring(0, 2) + str + str.substring(0, 2);
+    }
+
+    public boolean startHi(String str) {
+        if (str.length() < 2) return false;
+        return str.substring(0, 2).equals("hi");
+    }
+
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+    }
+
+    public boolean in1020(int a, int b) {
+        boolean isAIn = a >= 10 && a <= 20;
+        boolean isBIn = b >= 10 && b <= 20;
+        return isAIn || isBIn;
+    }
+
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19) ||
+                (b >= 13 && b <= 19) ||
+                (c >= 13 && c <= 19);
+    }
+
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = a >= 13 && a <= 19;
+        boolean bTeen = b >= 13 && b <= 19;
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
+
+    public String delDel(String str) {
+        if (str.length() < 3) return str;
+        int index = str.indexOf("del");
+        if (index == 1) return str.substring(0, index) + str.substring(index + 3);
+        else return str;
+    }
+
+    public boolean mixStart(String str) {
+        if (str.length() < 3) return false;
+        return str.startsWith("ix", 1);
+    }
+
 }
