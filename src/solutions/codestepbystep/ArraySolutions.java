b7bd1fcb8cd12478ad3eb0d0b90da9133a4bbedf
stohirov <suxrobtoxirov001@gmail.com>
2025-01-27 00:57:34 +0500

    codestepbystep

---
+++ src/solutions/codestepbystep/ArraySolutions.java
@@ -0,0 +1,14 @@
+package solutions.codestepbystep;
+
+public class ArraySolutions {
+    public boolean contains(int[] nums1, int[] nums2) {
+        if (nums1.length < nums2.length) return false;
+
+        for (int i = 0; i < nums1.length - 1; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+
+            }
+        }
+        return false;
+    }
+}
