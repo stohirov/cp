61af097531f1b8f034b83f5995d0d9d1ea32bc3c
stohirov <suxrobtoxirov001@gmail.com>
2025-01-26 17:18:52 +0500

    Algo 0001

---
+++ src/solutions/algo/Solution1.java
@@ -0,0 +1,11 @@
+package solutions.algo;
+
+import java.util.Scanner;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println((int) Math.pow(number, 3));
+    }
+}
---
+++ src/solutions/leetcode/Solution14.java
@@ -0,0 +1,11 @@
+package solutions.leetcode;
+
+public class Solution14 {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        for (int i = 0; i < strs.length; i++) {
+
+        }
+        return result;
+    }
+}
