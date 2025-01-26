09c648f09c192bfb1605dd3056b7e5882f046d0f
stohirov <suxrobtoxirov001@gmail.com>
2025-01-26 17:44:15 +0500

    Algo 0004

---
+++ src/solutions/algo/Solution4.java
@@ -0,0 +1,11 @@
+package solutions.algo;
+
+import java.util.Scanner;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double r = scanner.nextDouble();
+        System.out.printf("%.2f", (4 * Math.PI * r * r));
+    }
+}
