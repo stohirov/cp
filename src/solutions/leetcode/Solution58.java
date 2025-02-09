f337b4b61e1d394d9899bd37c6576335fae160b2
stohirov <suxrobtoxirov001@gmail.com>
2025-02-09 23:23:00 +0500

    deleted

--- src/solutions/leetcode/Solution58.java
+++
@@ -1,8 +0,0 @@
-package solutions.leetcode;
-
-public class Solution58 {
-    public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split(" ");
-        return words[words.length - 1].length();
-    }
-}
