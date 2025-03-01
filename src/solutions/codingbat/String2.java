b396d6542fac990707a0b55077298377a435b2e3
stohirov <suxrobtoxirov001@gmail.com>
2025-03-01 15:49:00 +0500

    Cat Dog

--- src/solutions/codingbat/String2.java
+++ src/solutions/codingbat/String2.java
@@ -21,4 +21,19 @@ public class String2 {
         return count;
     }
 
+    public boolean catDog(String str) {
+        int length = str.length();
+        int countCat = 0;
+        int countDog = 0;
+        for (int i = 0; i < length-2; i++) {
+            if (str.substring(i, i + 3).equals("cat")) {
+                countCat++;
+            } else if (str.substring(i, i + 3).equals("dog")) {
+                countDog++;
+            }
+        }
+        return countCat == countDog;
+    }
+
+
 }
