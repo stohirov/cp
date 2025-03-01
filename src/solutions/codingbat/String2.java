1a4c2a4eff5ccc4e0e7d4a4f1f963e5d8413463c
stohirov <suxrobtoxirov001@gmail.com>
2025-03-01 15:48:13 +0500

    Count Hi

--- src/solutions/codingbat/String2.java
+++ src/solutions/codingbat/String2.java
@@ -5,8 +5,20 @@ public class String2 {
         String sum = "";
         int length = str.length();
         for (int i = 0; i < length; i++) {
-            sum += str.substring(i,i+1) + str.substring(i,i+1);
+            sum += str.charAt(i) + str.substring(i,i+1);
         }
         return sum;
     }
+
+    public int countHi(String str) {
+        int length = str.length();
+        int count = 0;
+        for (int i = 0; i < length-1; i++) {
+            if (str.substring(i, i + 2).equals("hi")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
