7be9df554e060b18ca793d7b853f0f96adb871e5
stohirov <suxrobtoxirov001@gmail.com>
2025-06-08 15:45:17 +0500

    ugly number

---
+++ src/solutions/leetcode/Solution263.java
@@ -0,0 +1,15 @@
+package solutions.leetcode;
+
+public class Solution263 {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+
+        while (n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
+            if (n % 2 == 0) n /= 2;
+            else if (n % 3 == 0) n /= 3;
+            else n /= 5;
+        }
+
+        return n == 1;
+    }
+}
