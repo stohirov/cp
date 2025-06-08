ab883548920ac0ad33f305de634c2c5e789da2c5
stohirov <suxrobtoxirov001@gmail.com>
2025-06-08 16:41:50 +0500

    power of four

---
+++ src/solutions/leetcode/Solution342.java
@@ -0,0 +1,9 @@
+package solutions.leetcode;
+
+public class Solution342 {
+    public boolean isPowerOfFour(int n) {
+        if (n < 1) return false;
+        while (n % 4 == 0) n /= 4;
+        return n == 1;
+    }
+}
