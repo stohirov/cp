390c9dc4142a625f45abd8cb01ee1a9be409eb94
Khuma77 <xasanxojayevhumoyun@gmail.com>
2025-06-24 13:37:43 +0500

    isPalindrome

---
+++ src/solutions/leetcode/Solution234.java
@@ -0,0 +1,26 @@
+package solutions.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution234 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        Stack<Integer> stack = new Stack<>();
+
+        while (fast != null && fast.next != null) {
+            stack.add(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        while (slow != null) {
+            if (!stack.isEmpty() && slow.val == stack.peek()) stack.pop();
+            slow = slow.next;
+        }
+
+        return stack.isEmpty();
+    }
+}
